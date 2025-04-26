a8e1637e8a8e2cb818787dce0c1afc969975f6a2
vaishnavisiddesh <120649838+vaishnavisiddesh@users.noreply.github.com>
2025-04-26 13:13:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
-$conn = mysqli_connect("localhost", "root", "", "reglog");
+$conn = mysqli_connect("aws-simplified.cr4ooq6sa891.eu-north-1.rds.amazonaws.com", "admin", "nithya2002", "reglog");
 ?>
